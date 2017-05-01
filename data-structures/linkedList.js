9c9a4c0f4e34383d80f631a53666a79469a8e337
Jonathon Lin <jonathonlin93@yahoo.com>
2017-04-30 17:44:32 -0700

    delete a node

---
+++ data-structures/linkedList.js
@@ -0,0 +1,71 @@
+// Print the Elements of a Linked List
+function print(head) {
+    if (head !== null){
+        console.log(head.data)
+        print(head.next);
+    }
+}
+
+// Insert a Node at the Tail of a Linked List
+function insert(head, data) {
+    if (head === null){
+        return new Node(data);
+    } else {
+        if (head.next === null){
+            head.next = new Node(data);
+        } else {
+            insert(head.next, data);
+        }
+    }
+    return head;
+}
+
+// Insert a node at the head of a linked list
+function insert(head, data) {
+    var node = new Node(data);
+    if (head !== null){
+        node.next = head;
+    }
+
+    return node;
+}
+
+// Insert a node at a specific position in a linked list
+function insert(head, data, position) {
+    var node = new Node(data);
+
+    if(head === null) {
+        return node;
+    }
+
+    if (position === 0){
+        node.next = head;
+        return node;
+    }
+
+    var current = head;
+    var i = 1;
+
+    while ( i < position) {
+        current = current.next;
+        i++;
+    }
+
+    node.next = current.next;
+    current.next = node;
+
+    return head;
+}
+
+// Delete a Node
+function deleteNode(head, position) {
+    var current = head;
+    if (position === 0){
+        return current.next;
+    }
+    for (var i = 0; i<position-1; i++){
+        current = current.next;
+    }
+    current.next = current.next.next;
+    return head;    
+}
