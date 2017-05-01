83aec6e6dd67388920743d8ca1abd5c94537cf79
Jonathon Lin <jonathonlin93@yahoo.com>
2017-04-30 21:56:13 -0700

    merge two sorted linked lists

--- data-structures/arrays.js
+++ data-structures/arrays.js
@@ -11,23 +11,23 @@ function main() {
 
 //2D Array - DS
 function main() {
-    var arr = [];
-    for(arr_i = 0; arr_i < 6; arr_i++){
-       arr[arr_i] = readLine().split(' ');
-       arr[arr_i] = arr[arr_i].map(Number);
-    }
+  var arr = [];
+  for(arr_i = 0; arr_i < 6; arr_i++){
+     arr[arr_i] = readLine().split(' ');
+     arr[arr_i] = arr[arr_i].map(Number);
+  }
 
-    var max = findSum(arr, 0, 0);
+  var max = findSum(arr, 0, 0);
 
-    for (var i=0; i<4; i++){
-        for (var j=0; j<4; j++){
-            var temp = findSum(arr,i,j);
-           if (max < temp) {
-               max = temp;
-           }
-        }
-    }
-    process.stdout.write(max.toString());
+  for (var i=0; i<4; i++){
+      for (var j=0; j<4; j++){
+          var temp = findSum(arr,i,j);
+         if (max < temp) {
+             max = temp;
+         }
+      }
+  }
+  process.stdout.write(max.toString());
 }
 
 function findSum(arr, i, j){
@@ -36,62 +36,62 @@ function findSum(arr, i, j){
 
 // Dynamic Array
 function processData(input) {
-    var tempInputs = input.split("\n");
-    var inputs = tempInputs.map(function(el) {
-        return el.split(" ").map(Number);
-    });
-    var lastAns = 0;
-    var n = inputs[0][0];
-    var q = inputs[0][1];
-    var seqList = [];
-    for (var i=1; i<=n; i++) {
-        seqList.push([]);
-    }
-    for (var i=1; i<=q; i++) {
-        if (inputs[i][0] === 1){
-            queryOne(seqList, n, lastAns, inputs[i][1], inputs[i][2]);
-        } else {
-            lastAns = queryTwo(seqList, n, lastAns, inputs[i][1], inputs[i][2]);
-            console.log(lastAns);
-        }
-    }
+  var tempInputs = input.split("\n");
+  var inputs = tempInputs.map(function(el) {
+      return el.split(" ").map(Number);
+  });
+  var lastAns = 0;
+  var n = inputs[0][0];
+  var q = inputs[0][1];
+  var seqList = [];
+  for (var i=1; i<=n; i++) {
+      seqList.push([]);
+  }
+  for (var i=1; i<=q; i++) {
+      if (inputs[i][0] === 1){
+          queryOne(seqList, n, lastAns, inputs[i][1], inputs[i][2]);
+      } else {
+          lastAns = queryTwo(seqList, n, lastAns, inputs[i][1], inputs[i][2]);
+          console.log(lastAns);
+      }
+  }
 }
 
 //Left Rotation
 function processData(input) {
-    var tempInput = input.split("\n");
-    var inputs = tempInput.map(function(el){
-        return el.split(" ").map(Number);
-    });
-    var n = inputs[0][0];
-    var d = inputs[0][1];
-    var point = calcNewPosition(n,d);
-    var newArr = inputs[1].slice(point,n).concat(inputs[1].slice(0,point));
-    var ans = newArr.join(" ");
+  var tempInput = input.split("\n");
+  var inputs = tempInput.map(function(el){
+      return el.split(" ").map(Number);
+  });
+  var n = inputs[0][0];
+  var d = inputs[0][1];
+  var point = calcNewPosition(n,d);
+  var newArr = inputs[1].slice(point,n).concat(inputs[1].slice(0,point));
+  var ans = newArr.join(" ");
 
-    console.log(ans);
+  console.log(ans);
 }
 
 function calcNewPosition(n,d) {
-    return d%n;
+  return d%n;
 }
 
 // Sparse Arrays
 function processData(input) {
-    var inputs = input.split("\n");
-    var n = parseInt(inputs[0]);
-    var q = parseInt(inputs[n+1]);
-    var words = {};
+  var inputs = input.split("\n");
+  var n = parseInt(inputs[0]);
+  var q = parseInt(inputs[n+1]);
+  var words = {};
 
-    for (var i=1; i<=n; i++) {
-        if (words[inputs[i]] >= 1) {
-            words[inputs[i]] += 1;
-        } else {
-            words[inputs[i]] = 1;
-        }
-    }
+  for (var i=1; i<=n; i++) {
+      if (words[inputs[i]] >= 1) {
+          words[inputs[i]] += 1;
+      } else {
+          words[inputs[i]] = 1;
+      }
+  }
 
-    for (var i=1; i<=q; i++) {
-        console.log(words[inputs[(n+1)+i]]||0);
-    }
+  for (var i=1; i<=q; i++) {
+      console.log(words[inputs[(n+1)+i]]||0);
+  }
 }
--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -1,112 +1,135 @@
 // Print the Elements of a Linked List
 function print(head) {
-    if (head !== null){
-        console.log(head.data)
-        print(head.next);
-    }
+  if (head !== null){
+      console.log(head.data)
+      print(head.next);
+  }
 }
 
 // Insert a Node at the Tail of a Linked List
 function insert(head, data) {
-    if (head === null){
-        return new Node(data);
-    } else {
-        if (head.next === null){
-            head.next = new Node(data);
-        } else {
-            insert(head.next, data);
-        }
-    }
-    return head;
+  if (head === null){
+      return new Node(data);
+  } else {
+      if (head.next === null){
+          head.next = new Node(data);
+      } else {
+          insert(head.next, data);
+      }
+  }
+  return head;
 }
 
 // Insert a node at the head of a linked list
 function insert(head, data) {
-    var node = new Node(data);
-    if (head !== null){
-        node.next = head;
-    }
+  var node = new Node(data);
+  if (head !== null){
+      node.next = head;
+  }
 
-    return node;
+  return node;
 }
 
 // Insert a node at a specific position in a linked list
 function insert(head, data, position) {
-    var node = new Node(data);
+  var node = new Node(data);
 
-    if(head === null) {
-        return node;
-    }
+  if(head === null) {
+      return node;
+  }
 
-    if (position === 0){
-        node.next = head;
-        return node;
-    }
+  if (position === 0){
+      node.next = head;
+      return node;
+  }
 
-    var current = head;
-    var i = 1;
+  var current = head;
+  var i = 1;
 
-    while ( i < position) {
-        current = current.next;
-        i++;
-    }
+  while ( i < position) {
+      current = current.next;
+      i++;
+  }
 
-    node.next = current.next;
-    current.next = node;
+  node.next = current.next;
+  current.next = node;
 
-    return head;
+  return head;
 }
 
 // Delete a Node
 function deleteNode(head, position) {
-    var current = head;
-    if (position === 0){
-        return current.next;
-    }
-    for (var i = 0; i<position-1; i++){
-        current = current.next;
-    }
-    current.next = current.next.next;
-    return head;
+  var current = head;
+  if (position === 0){
+      return current.next;
+  }
+  for (var i = 0; i<position-1; i++){
+      current = current.next;
+  }
+  current.next = current.next.next;
+  return head;
 }
 
 // Print in Reverse
 function reversePrint(head) {
-    var nodeList = [];
-    var node = head;
+  var nodeList = [];
+  var node = head;
 
 
-    while (node !== null){
-        nodeList.push(node.data);
-        node = node.next;
-    }
+  while (node !== null){
+      nodeList.push(node.data);
+      node = node.next;
+  }
 
-    while (nodeList.length > 0){
-        console.log(nodeList.pop());
-    }
+  while (nodeList.length > 0){
+      console.log(nodeList.pop());
+  }
 }
 
 // Compare two linked lists
 function compareLinkedLists( headA, headB) {
-    var nodeA = headA;
-    var nodeB = headB;
+  var nodeA = headA;
+  var nodeB = headB;
 
 
-    if (nodeA === null || nodeB === null) {
-        return 0;
+  if (nodeA === null || nodeB === null) {
+      return 0;
+  }
+  if(nodeA !== null && nodeB !== null){
+      while(nodeA.data === nodeB.data){
+         if(nodeA.next === null){
+            if(nodeB.next === null){
+               return 1;
+             }else{
+               return 0;
+            }
+         }
+
+         nodeA = nodeA.next;
+         nodeB = nodeB.next;
+      }
+  }
+  return 0;
+}
+
+// Merge two sorted linked lists
+function mergeLinkedLists( headA, headB) {
+    if (headA === null && headB === null){
+        return null;
     }
-    if(nodeA !== null && nodeB !== null){
-        while(nodeA.data === nodeB.data){
-           if(nodeA.next === null){
-              if(nodeB.next === null){
-                 return 1;
-               }else{
-                 return 0;
-              }
-           }
-
-           nodeA = nodeA.next;
-           nodeB = nodeB.next;
-        }
+    if (headA === null){
+        return headB;
     }
-    return 0;
+    if (headB === null) {
+        return headA;
+    }
+
+    if (headA.data < headB.data){
+        headA.next = mergeLinkedLists(headA.next, headB);
+        return headA;
+    }
+    else {
+        headB.next = mergeLinkedLists(headA, headB.next);
+        return headB;
+    }
+}
